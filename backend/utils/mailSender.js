ff392928baaa738ab61709171d6acb711f7c2792
Nitin <beldarnitin845@gmail.com>
2026-07-14 20:26:23 +0530

    The mail sender error are fixing

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -17,7 +17,7 @@ const mailSender = async (email, title, body) => {
     console.log("SMTP verified successfully");
 
     let info = await transporter.sendMail({
-      from: `StudyNotion || Codehelp - by Nitin`,
+      from: `StudyNotion || Codehelp - by Nitin <${process.env.MAIL_USER}>`,
       to: `${email}`,
       subject: `${title}`,
       html: `${body}`,
